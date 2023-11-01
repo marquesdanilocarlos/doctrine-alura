52e8c014736ff9143e3447f0e6cb35bd7522649e
Danilo Carlos Marques <danilo.marques@basis.com.br>
2023-11-01 10:44:53 -0300

    Aula 15 - Query Builder

--- src/Repositories/StudentRepository.php
+++ src/Repositories/StudentRepository.php
@@ -8,13 +8,21 @@ class StudentRepository extends EntityRepository
 {
     public function getStudentsAndCourses()
     {
-        $students = $this->_em->createQuery(
+        return $this->createQueryBuilder('student')
+            ->addSelect('phone')
+            ->addSelect('course')
+            ->leftJoin('student.phones', 'phone')
+            ->leftJoin('student.courses', 'course')
+            ->getQuery()
+            ->getResult();
+
+        /*$students = $this->_em->createQuery(
             "SELECT student, phone, course 
             FROM {$this->getEntityName()} student 
             LEFT JOIN student.phones phone 
             LEFT JOIN student.courses course"
         )->getResult();
 
-        return $students;
+        return $students;*/
     }
 }
